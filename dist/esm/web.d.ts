8cfbb07baa894809b995701e26815d1ace983c87
Manuel Rodríguez <mrodriguez@whiteguru.com>
2021-11-01 12:34:50 +0100

    Remove dist folder before publish to npmjs

--- dist/esm/web.d.ts
+++
@@ -1,5 +0,0 @@
-import { WebPlugin } from '@capacitor/core';
-import type { FilePickerOptions, FilePickerPlugin, FilePickerResults } from './definitions';
-export declare class FilePickerWeb extends WebPlugin implements FilePickerPlugin {
-    pick(options: FilePickerOptions): Promise<FilePickerResults>;
-}
--- dist/esm/web.js
+++
@@ -1,8 +0,0 @@
-import { WebPlugin } from '@capacitor/core';
-export class FilePickerWeb extends WebPlugin {
-    async pick(options) {
-        console.log('pick', options);
-        throw this.unimplemented('Not implemented on web.');
-    }
-}
-//# sourceMappingURL=web.js.map
